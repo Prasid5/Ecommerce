ec2e37953199ace65d0bf53fcd15d9e0ab098152
Prasid <prasidshahi5@gmail.com>
2025-12-13 09:46:29 +0545

    For Render

--- Sneakee/settings.py
+++ Sneakee/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Sneakee.onrender.com", "localhost", "127.0.0.1"]
+
 
 
 # Application definition
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,7 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[BASE_DIR/"static"]
+STATICFILES_DIRS = [BASE_DIR / "static"]  # your source static folder
+STATIC_ROOT = BASE_DIR / "staticfiles"    # folder where collectstatic will put files
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/"media"
 
