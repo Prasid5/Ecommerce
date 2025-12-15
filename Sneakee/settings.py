5b0efcde72c5c0990888f5c0c5e49a7e954a9282
Prasid <prasidshahi5@gmail.com>
2025-12-15 11:14:03 +0545

    Solving the image load problem

--- Sneakee/settings.py
+++ Sneakee/settings.py
@@ -131,12 +131,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'  # Add this line
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = '/static/media/'
 MEDIA_ROOT = BASE_DIR / "staticfiles" / "media"
-STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "media"]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
